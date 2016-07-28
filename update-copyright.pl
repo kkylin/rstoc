86360db327f4aafc3dc2b59e31d30b4766293f3f
Kevin K Lin <klin@math.arizona.edu>
2016-07-28 10:24:22 -0700

    update-copyright.pl: also update bruce email

--- update-copyright.pl
+++ update-copyright.pl
@@ -2,8 +2,19 @@
 
 foreach $file (@ARGV) {
     $_ = `cat $file`;
-    if ( /Copyright \(C\) 2012 by the authors/gs ) {
-	s/Copyright \(C\) 2012 by the authors/Copyright (C) 2012,2013,2014,2015,2016 by the authors/gs;
+
+    # if ( /Copyright \(C\) 2012 by the authors/gs ) {
+    # 	s/Copyright \(C\) 2012 by the authors/Copyright (C) 2012,2013,2014,2015,2016 by the authors/gs;
+
+    # 	$outfile = "${file}.out";
+    # 	open(FOUT, ">$outfile");
+    # 	print FOUT "$_";
+    # 	close(FOUT);
+    # 	system("mv -f $outfile $file");
+    # }
+
+    if ( /bruce\@math.duke.edu/gs ) {
+	s/bruce\@math.duke.edu/bruce.warren.rogers\@gmail.com/gs;
 
 	$outfile = "${file}.out";
 	open(FOUT, ">$outfile");
@@ -12,4 +23,3 @@ foreach $file (@ARGV) {
 	system("mv -f $outfile $file");
     }
 }
-    
