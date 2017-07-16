e7cd4ead801a17e50fc8ef013e8d631e2f952017
K K Lin <klin@math.arizona.edu>
2017-07-15 22:31:59 -0700

    update-copyright.pl: bump copyright

--- update-copyright.pl
+++ update-copyright.pl
@@ -3,8 +3,18 @@
 foreach $file (@ARGV) {
     $_ = `cat $file`;
 
-    # if ( /Copyright \(C\) 2012 by the authors/gs ) {
-    # 	s/Copyright \(C\) 2012 by the authors/Copyright (C) 2012,2013,2014,2015,2016 by the authors/gs;
+    if ( /Copyright \(C\).*by the authors/gs ) {
+    	s/(Copyright \(C\).*?) by the authors/$1,2017 by the authors/gs;
+
+    	$outfile = "${file}.out";
+    	open(FOUT, ">$outfile");
+    	print FOUT "$_";
+    	close(FOUT);
+    	system("mv -f $outfile $file");
+    }
+
+    # if ( /bruce\@math.duke.edu/gs ) {
+    # 	s/bruce\@math.duke.edu/bruce.warren.rogers\@gmail.com/gs;
 
     # 	$outfile = "${file}.out";
     # 	open(FOUT, ">$outfile");
@@ -12,14 +22,4 @@ foreach $file (@ARGV) {
     # 	close(FOUT);
     # 	system("mv -f $outfile $file");
     # }
-
-    if ( /bruce\@math.duke.edu/gs ) {
-	s/bruce\@math.duke.edu/bruce.warren.rogers\@gmail.com/gs;
-
-	$outfile = "${file}.out";
-	open(FOUT, ">$outfile");
-	print FOUT "$_";
-	close(FOUT);
-	system("mv -f $outfile $file");
-    }
 }
